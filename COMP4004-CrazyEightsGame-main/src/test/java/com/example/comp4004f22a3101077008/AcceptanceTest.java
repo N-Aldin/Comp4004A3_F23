adc20cfc0bfcd331962280a749d0d4c36a8540ca
Aldin Newaz <aldin.newaz@outlook.com>
2023-12-08 01:18:18 -0500

    Added test row 29
    p4 plays 3C: assert next player is player 1

--- COMP4004-CrazyEightsGame-main/src/test/java/com/example/comp4004f22a3101077008/AcceptanceTest.java
+++ COMP4004-CrazyEightsGame-main/src/test/java/com/example/comp4004f22a3101077008/AcceptanceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -28,6 +30,7 @@ public class AcceptanceTest {
     GameLogic game;
     @LocalServerPort
     int port;
+    int[] pCardStrtInd = {2, 7, 12, 17, 22};
 
 //    Browsers
     ArrayList<WebDriver> browsers;
@@ -59,6 +62,20 @@ public class AcceptanceTest {
             browsers.add(driver);
         }
 
+
+        // RESIZE WINDOWS
+        int width = 1920/2;
+        int height = (1080)/2;
+
+        browsers.get(0).manage().window().setPosition(new org.openqa.selenium.Point(0, 0));
+        browsers.get(0).manage().window().setSize(new Dimension(width, height));
+        browsers.get(1).manage().window().setPosition(new org.openqa.selenium.Point(width, 0));
+        browsers.get(1).manage().window().setSize(new Dimension(width, height));
+        browsers.get(2).manage().window().setPosition(new org.openqa.selenium.Point(0, height));
+        browsers.get(2).manage().window().setSize(new Dimension(width, height));
+        browsers.get(3).manage().window().setPosition(new org.openqa.selenium.Point(width, height));
+        browsers.get(3).manage().window().setSize(new Dimension(width, height));
+
     }
 
     @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
@@ -69,12 +86,6 @@ public class AcceptanceTest {
         rigTestRow25();
 
         browsers.get(0).findElement(By.id("startBtn")).click();
-
-//        try {
-//            TimeUnit.MILLISECONDS.sleep(5000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
 
         browsers.get(0).findElement(By.id("3C")).click();
 
@@ -214,10 +225,133 @@ public class AcceptanceTest {
             }
         }
 
-//        System.out.println(rCard.size());
-//        for (int i = 0; i < rCard.size(); ++i){
-//            System.out.println(rCard.get(i).getSuit() + rCard.get(i).getRank());
-//        }
+        gd.setCards(rCard);
+        gd.setTopCard(game.startGame(gd.getCards(), gd.getPlayers()));
+    }
+
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    @Test
+    @DisplayName("p1 plays QC assert next player is player 3  (because player 2 is notified and loses their turn)")
+    public void TestRow28() throws InterruptedException {
+
+        rigTestRow28();
+
+        browsers.get(0).findElement(By.id("startBtn")).click();
+
+        browsers.get(0).findElement(By.id("QC")).click();
+
+        // Check the player turn is 4 for all the browsers and the direction is going left
+        for (int i = 0; i < 4; ++i){
+            assert (browsers.get(i).findElement(By.id("turnID")).getText().contains("3"));
+        }
+
+        assert (browsers.get(2).findElement(By.id("draw")).isEnabled());
+
+        TimeUnit.MILLISECONDS.sleep(5000);
+    }
+
+    // Need player 1 to have QC
+    public void rigTestRow28(){
+        String [] suit = {"S","C","D","H"};
+        String [] rank = {"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
+
+        ArrayList<Card> rCard = new ArrayList<>();
+        int counter = 1;
+
+        // Will be top card
+        rCard.add(new Card("C", "4"));
+
+        // Will be dealt to player 1
+        rCard.add(new Card("C", "Q"));
+
+        for (String s : suit) {
+            for (String value : rank) {
+                // skip the card that was added to be dealt to player 1
+                if (s.equals("C") && value.equals("Q")) continue;
+                    // chose a random club card to be the top card
+                else if (s.equals("C") && value.equals("4")) continue;
+
+                counter++;
+
+                Card c = new Card(s, value);
+                rCard.add(c);
+            }
+        }
+
+        gd.setCards(rCard);
+        gd.setTopCard(game.startGame(gd.getCards(), gd.getPlayers()));
+    }
+
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    @Test
+    @DisplayName("p4 plays 3C: assert next player is player 1")
+    public void TestRow29() throws InterruptedException {
+        rigTestRow29();
+
+        browsers.get(0).findElement(By.id("startBtn")).click();
+
+        // play cards
+        browsers.get(0).findElement(By.id("3C")).click();
+        TimeUnit.MILLISECONDS.sleep(1000);
+
+        browsers.get(1).findElement(By.id("4C")).click();
+        TimeUnit.MILLISECONDS.sleep(1000);
+
+        browsers.get(2).findElement(By.id("5C")).click();
+        TimeUnit.MILLISECONDS.sleep(1000);
+
+        browsers.get(3).findElement(By.id("6C")).click();
+        TimeUnit.MILLISECONDS.sleep(50000);
+
+        // Check the player turn is 4 for all the browsers and the direction is going left
+        for (int i = 0; i < 4; ++i){
+            assert (browsers.get(i).findElement(By.id("turnID")).getText().contains("1"));
+        }
+
+        assert (browsers.get(0).findElement(By.id("draw")).isEnabled());
+
+    }
+
+    public void rigTestRow29(){
+        String [] suit = {"S","C","D","H"};
+        String [] rank = {"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
+
+        ArrayList<Card> rCard = new ArrayList<>();
+        int counter = 1;
+
+        // Will be top card
+        rCard.add(new Card("C", "A"));
+
+        // Will be dealt to player 1
+        rCard.add(new Card("C", "3"));
+
+        for (String s : suit) {
+            for (String value : rank) {
+                // skip the card that was added to be dealt to player 1
+                if (s.equals("C") && value.equals("A")) continue;
+                    // chose a random club card to be the top card
+                else if (s.equals("C") && value.equals("3")) continue;
+                else if (s.equals("C") && value.equals("4")) continue;
+                else if (s.equals("C") && value.equals("5")) continue;
+                else if (s.equals("C") && value.equals("6")) continue;
+
+
+                counter++;
+                if (counter == 7){
+                    rCard.add(new Card("C", "4"));
+                    continue;
+                }else if (counter == 12){
+                    rCard.add(new Card("C", "5"));
+                    continue;
+                }else if (counter == 17){
+                    rCard.add(new Card("C", "6"));
+                    continue;
+                }
+
+                Card c = new Card(s, value);
+                rCard.add(c);
+            }
+        }
 
         gd.setCards(rCard);
         gd.setTopCard(game.startGame(gd.getCards(), gd.getPlayers()));
