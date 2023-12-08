26eafc14abbcbb78ea001b429f061a1e3310ab78
Aldin Newaz <aldin.newaz@outlook.com>
2023-12-07 22:33:02 -0500

    Added row 26 test
    Added test for row 26 testing playing of an ace which reverses the order

--- COMP4004-CrazyEightsGame-main/src/test/java/com/example/comp4004f22a3101077008/AcceptanceTest.java
+++ COMP4004-CrazyEightsGame-main/src/test/java/com/example/comp4004f22a3101077008/AcceptanceTest.java
@@ -61,6 +61,7 @@ public class AcceptanceTest {
 
     }
 
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     @Test
     @DisplayName("p1 plays 3C: assert next player is player 2")
     public void TestRow25(){
@@ -77,9 +78,12 @@ public class AcceptanceTest {
 
         browsers.get(0).findElement(By.id("3C")).click();
 
-        // Check the player turn is 2 for all the browsers
+        // TODO: Check the top card ******
+
+        // Check the player turn is 2 for all the browsers and the direction is going right
         for (int i = 0; i < 4; ++i){
             assert (browsers.get(i).findElement(By.id("turnID")).getText().contains("2"));
+            assert (browsers.get(i).findElement(By.id("direction")).getText().contains("left"));
         }
 
         assert browsers.get(1).findElement(By.id("draw")).isEnabled();
@@ -129,10 +133,55 @@ public class AcceptanceTest {
         gd.setTopCard(game.startGame(gd.getCards(), gd.getPlayers()));
     }
 
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     @Test
     @DisplayName("p1 plays 1H assert next player is player 4 AND interface must show now playing in opposite direction (i.e., going right) " +
             "player4 plays 7H and next player is player 3")
     public void TestRow26(){
+
+        rigTestRow26();
+
+        browsers.get(0).findElement(By.id("startBtn")).click();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        browsers.get(0).findElement(By.id("AH")).click();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Check the player turn is 4 for all the browsers and the direction is going left
+        for (int i = 0; i < 4; ++i){
+            assert (browsers.get(i).findElement(By.id("turnID")).getText().contains("4"));
+            assert (browsers.get(i).findElement(By.id("direction")).getText().contains("right"));
+        }
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // check the players draw button is enabled
+        assert browsers.get(3).findElement(By.id("draw")).isEnabled();
+
+        browsers.get(3).findElement(By.id("7H")).click();
+
+        // checking that player 3's draw button is enabled signifying that it is their turn
+        assert browsers.get(2).findElement(By.id("draw")).isEnabled();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -148,16 +197,17 @@ public class AcceptanceTest {
         rCard.add(new Card("H", "4"));
 
         // Will be dealt to player 1
-        rCard.add(new Card("C", "3"));
+        rCard.add(new Card("H", "A"));
 
         for (String s : suit) {
             for (String value : rank) {
                 // skip the card that was added to be dealt to player 1
-                if (s.equals("C") && value.equals("3")) continue;
+                if (s.equals("H") && value.equals("A")) continue;
                     // chose a random club card to be the top card
-                else if (s.equals("C") && value.equals("4")) continue;
+                else if (s.equals("H") && value.equals("4")) continue;
 
                 counter++;
+                if (counter == 17) rCard.add(new Card("H", "7"));
 
                 Card c = new Card(s, value);
                 rCard.add(c);
